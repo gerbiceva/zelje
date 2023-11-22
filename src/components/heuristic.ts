0a92c415ef0db52f63d603f82874bc8e5f59f583
Lan Vukušič <info@lanvukusic.com>
2023-11-22 20:59:04 +0100

    speedddduuuppp

--- src/components/heuristic.ts
+++ src/components/heuristic.ts
@@ -1,7 +1,5 @@
 export function heuristic(elapsedTimeSec: number, clicks: number) {
-  return clicks - elapsedTimeSec / (10.2 * 60) ** 0.9;
-  // return clicks;
-  // return clicks * 4 - ((elapsedTimeSec / 10) * 5) ** 0.8;
+  return clicks;
 }
 
 export function score(time: string, clicks: number, nowSec: number) {
